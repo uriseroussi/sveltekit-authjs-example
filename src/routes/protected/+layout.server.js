823e4abcd88168326f09771e5aa3a1423629e836
Uri Seroussi <uriser@gmail.com>
2024-01-11 11:25:36 -0500

    update packages

--- src/routes/protected/+layout.server.js
+++ src/routes/protected/+layout.server.js
@@ -4,7 +4,7 @@ export const load = async (event) => {
 	const session = await event.locals.getSession();
 
 	if (!session) {
-		throw redirect(307, 'auth/signin');
+		redirect(307, 'auth/signin');
 	}
 
 	return {
